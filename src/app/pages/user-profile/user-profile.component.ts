f9b2d41d0794ae2e21c778748c5e6b9fd5fae002
rebpacquiao <reb.pacquiao@gmail.com>
2024-06-13 19:47:15 +0800

    refactor: Update user-profile.component.ts to import necessary modules and implement OnInit interface

--- src/app/pages/user-profile/user-profile.component.ts
+++ src/app/pages/user-profile/user-profile.component.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Observable } from 'rxjs';
+import { UsersService } from '../../services/users.service';
+import { User } from '../../model/users.model';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-user-profile',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatCardModule, MatButtonModule],
   templateUrl: './user-profile.component.html',
-  styleUrl: './user-profile.component.scss'
+  styleUrl: './user-profile.component.scss',
 })
 export class UserProfileComponent {
+  post$!: Observable<User>;
 
+  constructor(
+    private postsService: UsersService,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const postId = this.route.snapshot.params['id'];
+    this.post$ = this.postsService.getPostById(postId);
+  }
 }
