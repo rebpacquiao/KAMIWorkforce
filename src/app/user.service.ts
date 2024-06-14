f8aa842b14e2a3d94ee9ce78c667f6185a3d4ca7
rebpacquiao <reb.pacquiao@gmail.com>
2024-06-14 18:47:40 +0800

    refactor: Update setUserMetadata method to accept UserMetadata interface

--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,15 +1,22 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
+interface UserMetadata {
+  id: number;
+  name: string;
+  email: string;
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class UserService {
-  private userDataSubject = new BehaviorSubject<any>(null);
+  private userDataSubject = new BehaviorSubject<UserMetadata | null>(null);
   userData$ = this.userDataSubject.asObservable();
+
   constructor() {}
 
-  setUserMetadata(metadata: any) {
+  setUserMetadata(metadata: UserMetadata) {
     this.userDataSubject.next(metadata);
   }
 }
